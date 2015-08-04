6e0ff087688b38080c99e48ef78cbd7897f8582c
amine El houba <amine142@hotmail.fr>
2015-08-04 10:28:53 +0200

    commit joel

--- formulaire.php
+++ formulaire.php
@@ -19,6 +19,8 @@ injection sql et la pdo nouvelles façons d'envoyer des requête PHP 5: http://j
 
 
 
+
+
 $connexion = new PDO("mysql:dbname=base1;host=localhost", "root" , "");// à adapter selon le nom de la base crée
 $statement = $connexion->query("SELECT * FROM matiere;");
 $matieres = $statement->fetchAll();
